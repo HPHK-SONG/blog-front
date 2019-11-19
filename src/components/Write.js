09c953053a352d78039303278b338694678a13e4
HPHK-SONG <hphksong@gmail.com>
2019-11-19 11:45:29 +0900

    ~12:00

--- src/components/Write.js
+++ src/components/Write.js
@@ -1,5 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TextareaAutosize from 'react-textarea-autosize';
+import ReactMarkdown from 'react-markdown';
+import axios from 'axios';
+import { baseURL } from '../common/config';
 
 export default function Write() {
-  return <div>글쓰기화면</div>;
+  const [tag, setTag] = useState('');
+  const [tags, setTags] = useState([]);
+
+  const addTag = async () => {
+    const res = await axios.get(
+      `${baseURL}/api/tag/${tag}`,
+    );
+    if (res.data.error) {
+      const { data } = await axios.post(
+        `${baseURL}/api/tag`,
+        {
+          name: tag,
+        },
+      );
+      setTags([...tags, data.tag]);
+    } else {
+      setTags([...tags, res.data.tag]);
+    }
+  };
+
+  return (
+    <>
+      <label>태그 추가</label>
+      <input
+        type="text"
+        value={tag}
+        onChange={(e) => setTag(e.target.value)}
+      />
+      <button
+        type="button"
+        className="btn btn-success"
+        onClick={addTag}
+      >
+        태그 추가
+      </button>
+      <div>
+        {tags.map(({ name }) => (
+          <button
+            type="button"
+            className="btn btn-secondary"
+          >
+            {name}
+          </button>
+        ))}
+      </div>
+      <div className="write">
+        <div className="write-vertical">
+          <label>제목</label>
+          <input type="text" />
+          <label>내용</label>
+          <TextareaAutosize />
+          <button type="button" className="btn btn-primary">
+            제출
+          </button>
+        </div>
+        <div>
+          <ReactMarkdown />
+        </div>
+      </div>
+    </>
+  );
 }
