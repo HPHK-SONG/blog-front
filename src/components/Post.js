d5ec9cadd78028cba594c22cc077110f9125f5e0
HPHK-SONG <hphksong@gmail.com>
2019-11-19 13:56:20 +0900

    complete write

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import ReactMarkdown from 'react-markdown';
+
 import { baseURL } from '../common/config';
 
 export default function Post({ match }) {
@@ -15,7 +17,7 @@ export default function Post({ match }) {
   return (
     <>
       <h1>{post && post.title}</h1>
-      <p>{post && post.contents}</p>
+      {post && <ReactMarkdown source={post.contents} />}
     </>
   );
 }
--- src/components/Write.js
+++ src/components/Write.js
@@ -3,10 +3,13 @@ import TextareaAutosize from 'react-textarea-autosize';
 import ReactMarkdown from 'react-markdown';
 import axios from 'axios';
 import { baseURL } from '../common/config';
+import authAxios from '../common/authAxios';
 
-export default function Write() {
+export default function Write({ history }) {
   const [tag, setTag] = useState('');
   const [tags, setTags] = useState([]);
+  const [title, setTitle] = useState('');
+  const [contents, setContents] = useState('');
 
   const addTag = async () => {
     const res = await axios.get(
@@ -20,8 +23,32 @@ export default function Write() {
         },
       );
       setTags([...tags, data.tag]);
+      setTag('');
     } else {
       setTags([...tags, res.data.tag]);
+      setTag('');
+    }
+  };
+  const deleteTag = (i) => {
+    const newTags = [...tags];
+    newTags.splice(i, 1);
+    setTags(newTags);
+  };
+
+  const submit = async () => {
+    const { data } = await authAxios.post(
+      `${baseURL}/api/post`,
+      {
+        title,
+        contents,
+        tags: tags.map((t) => t._id),
+      },
+    );
+    if (data.result) history.push('/');
+    else {
+      alert(
+        '글이 등록되지 않음ㅜㅜ, 관리자=너 에게 문의하세요',
+      );
     }
   };
 
@@ -41,10 +68,11 @@ export default function Write() {
         태그 추가
       </button>
       <div>
-        {tags.map(({ name }) => (
+        {tags.map(({ name }, i) => (
           <button
             type="button"
             className="btn btn-secondary"
+            onClick={() => deleteTag(i)}
           >
             {name}
           </button>
@@ -53,15 +81,26 @@ export default function Write() {
       <div className="write">
         <div className="write-vertical">
           <label>제목</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
           <label>내용</label>
-          <TextareaAutosize />
-          <button type="button" className="btn btn-primary">
+          <TextareaAutosize
+            value={contents}
+            onChange={(e) => setContents(e.target.value)}
+          />
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={submit}
+          >
             제출
           </button>
         </div>
         <div>
-          <ReactMarkdown />
+          <ReactMarkdown source={contents} />
         </div>
       </div>
     </>
